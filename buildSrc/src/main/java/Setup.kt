d7117dbc1dd9ba1d871e2ea2694e6bfd8fd599f8
You Qi <youqi@axzae.com>
2023-12-05 14:41:33 +0800

    User buildSrc to store kts constants

---
+++ buildSrc/src/main/java/Setup.kt
@@ -0,0 +1,99 @@
+object Setup {
+    object Version {
+        // SDK
+        val minSdkVersion = 16
+        val brokerProjectMinSdkVersion = 24
+        val automationAppMinSDKVersion = 21
+        val targetSdkVersion = 33
+        val compileSdkVersion = 34
+        val buildToolsVersion = "28.0.3"
+        val versionName = "4.10.1"
+
+        // Plugins
+        val gradleVersion = "7.4.2"
+        val kotlinVersion = "1.7.21"
+        val spotBugsGradlePluginVersion = "4.7.1"
+        val jupiterApiVersion = "5.6.0"
+
+        // Java Language Support
+        val coreLibraryDesugaringVersion = "1.1.5"
+
+        // Libraries
+        val androidxTestRunnerVersion = "1.4.0"
+        val androidxTestOrchestratorVersion = "1.4.1"
+        val androidxTestMonitorVersion = "1.4.0"
+        val androidxTestCoreVersion = "1.4.0"
+        val androidxJunitVersion = "1.1.3"
+        val annotationVersion = "1.0.0"
+        val appCompatVersion = "1.1.0"
+        val browserVersion = "1.0.0"
+        val constraintLayoutVersion = "1.1.3"
+        val dexmakerMockitoVersion = "2.19.0"
+        val espressoCoreVersion = "3.1.0"
+        val gsonVersion = "2.8.9"
+        val junitVersion = "4.12"
+        val legacySupportV4Version = "1.0.0"
+        val localBroadcastManagerVersion = "1.0.0"
+        val lombokVersion = "1.18.22"
+        val materialVersion = "1.0.0"
+        val mockitoCoreVersion = "3.6.28"
+        val mockitoAndroidVersion = "3.6.28"
+        val multidexVersion = "2.0.1"
+        val nimbusVersion = "9.9"
+        val powerMockVersion = "2.0.9"
+        val runnerVersion = "1.2.0"
+        val rulesVersion = "1.2.0"
+        val robolectricVersion = "4.9.2"
+        val uiAutomatorVersion = "2.2.0"
+        val flatBuffersJavaVersion = "1.12.0"
+        val daggerVersion = "2.31.2"
+        val daggerCompilerVersion = "2.31.2"
+        val msalCommonVersion = "16.1.1"
+        val msal4jVersion = "1.10.0"
+        val mseberaApacheHttpClientVersion = "4.5.8"
+        val moshiVersion = "1.14.0"
+        val moshiAdaptersVersion = "1.14.0"
+        val openpojoVersion = "0.9.1"
+        val equalsVerifierVersion = "3.6.1"
+        val jsonGeneratorVersion = "0.4.7"
+        val jsonSchemaFriendVersion = "0.12.2"
+        val jsonVersion = "20231013"
+        val junitJupiterEngineVersion = "5.7.2"
+        val junitVintageEngineVersion = "5.7.2"
+        val dbusJavaVersion = "3.3.0"
+        val dbusJavaUtilsVersion = "3.3.0"
+        val bouncyCastleVersion = "1.69"
+        val oshiCoreVersion = "5.7.5"
+        val tokenShare = "1.6.5"
+        val intuneAppSdkVersion = "8.6.3"
+        val javaAssistVersion = "3.27.0-GA"
+        val yubikitAndroidVersion = "2.3.0"
+        val yubikitPivVersion = "2.3.0"
+        val powerliftAndroidVersion = "1.0.3"
+        val opentelemetryVersion = "1.18.0"
+
+        // microsoft-diagnostics-uploader app versions
+        val powerliftVersion = "0.14.7"
+        val cliktVersion = "3.4.2"
+        val okhttp3Version = "4.9.3"
+
+        // TODO: adal automation test app.
+        val supportLibraryVersion = "27.1.+"
+        val adalLegacy = "1.15.0"
+        val adalSnapshot = "3.1.1-SNAPSHOT"
+        val adalLatest = "3.1.1"
+
+        val projectCompileSdkVersion = 29
+        val projectBuildToolsVersion = "28.0.3"
+        val projectMinSdkVersion = 14
+        val projectTargetSdkVersion = 29
+        val projectVersionCode = 1
+        val projectVersionName = "1.5.9"
+        val junitLibraryVersion = "4.12"
+        val androidSupportLibraryVersion = "28.0.0"
+        val gsonLibraryVersion = "2.8.0"
+        val supportTestLibraryVersion = "0.4.1"
+        val mockitoLibraryVersion = "1.2"
+        val dexMakerLibraryVersion = "1.2"
+    }
+}
